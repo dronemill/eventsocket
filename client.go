afd2752d4ee58c8b6d8ed404b761f42b0d890514
Patrick McCarren <patrick@mmsc.io>
2015-03-28 18:54:23 -0400

    Added client creation

--- client.go
+++ client.go
@@ -1,7 +1,20 @@
 package eventsocket
 
 type Client struct {
-	Id uint64
+	Id string `json:"Id"`
 }
 
-type Clients []Client
+type Clients []*Client
+
+func newClient() (client *Client) {
+	client = new(Client)
+
+	id := <-uuidBuilder
+	client.Id = id.String()
+
+	clients = append(clients, client)
+
+	return
+}
+
+var clients = make(Clients, 16)
--- controller_client.go
+++ controller_client.go
@@ -1,16 +1,23 @@
 package eventsocket
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type controllerClient struct {
 	C *httpController
 }
 
+// handle a request to create a client
 func (C *controllerClient) Create(w http.ResponseWriter, r *http.Request) {
 	// log.Info("Creating a client")
 
 	// close the body fd when we exit
 	defer r.Body.Close()
 
-	panic("Awwww yeahhhh")
+	// create the client
+	client := newClient()
+
+	json.NewEncoder(w).Encode(client)
 }
