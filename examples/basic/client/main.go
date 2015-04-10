29de13def0ea8c89e633256a6e88d428a5b61a9a
Patrick McCarren <patrick@mmsc.io>
2015-04-09 21:16:39 -0400

    examples.basic: switch to using the go client

--- examples/basic/client/main.go
+++ examples/basic/client/main.go
@@ -3,66 +3,37 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"encoding/json"
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
 	"time"
 
-	"github.com/dronemill/eventsocket"
-	"github.com/gorilla/websocket"
+	"github.com/dronemill/eventsocket-client-go"
 )
 
-var d websocket.Dialer
 var writer = flag.Bool("writer", false, "Do we write to the websocket connection")
-
-func init() {
-	d = websocket.Dialer{
-		NetDial:          nil,
-		TLSClientConfig:  nil,
-		HandshakeTimeout: time.Second * 5,
-		ReadBufferSize:   4096,
-		WriteBufferSize:  4096,
-	}
-}
 
 func main() {
 	flag.Parse()
 
-	resp, err := http.Post("http://127.0.0.1:8080/v1/clients", "application/json", strings.NewReader(""))
+	client, err := eventsocketclient.NewClient("127.0.0.1:8080")
 	if err != nil {
 		panic(err)
 	}
 
-	defer resp.Body.Close()
-
-	client := eventsocket.Client{}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
+	if err := client.DialWs(); err != nil {
 		panic(err)
 	}
 
-	json.Unmarshal(body, &client)
-
-	headers := http.Header{}
-	c, _, err := d.Dial(fmt.Sprintf("ws://127.0.0.1:8080/v1/clients/%s/ws", client.Id), headers)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Connected!  %v  <====>  %v\n\n", c.RemoteAddr(), c.LocalAddr())
+	fmt.Println("Connected!")
 
 	if *writer {
-		go becomeWriter(c)
+		go becomeWriter(client)
 	}
 
-	subscribe(c)
+	client.Suscribe("foo")
 
 	for {
-		messageType, p, err := c.ReadMessage()
+		messageType, p, err := client.ReadMessage()
 		if err != nil {
 			panic(err)
 		}
@@ -71,34 +42,25 @@ func main() {
 	}
 }
 
-func subscribe(c *websocket.Conn) {
-	p := make(map[string]interface{})
-	p["Events"] = []string{"foo"}
-	m := eventsocket.Message{
-		MessageType: eventsocket.MESSAGE_TYPE_SUSCRIBE,
-		Payload:     p,
-	}
-	if err := c.WriteJSON(m); err != nil {
-		panic(err)
-	}
-}
-
-func becomeWriter(c *websocket.Conn) {
+func becomeWriter(client *eventsocketclient.Client) {
 	tickChan := time.NewTicker(time.Second).C
 
 	for {
 		select {
 		case <-tickChan:
-			p := make(map[string]interface{})
-			p["value"] = randomString(32)
-			m := eventsocket.Message{
-				MessageType: eventsocket.MESSAGE_TYPE_STANDARD,
-				Event:       "foo",
-				Payload:     p,
-			}
-			if err := c.WriteJSON(m); err != nil {
+			p := eventsocketclient.NewPayload()
+			p["awesomeValue"] = randomString(32)
+
+			if err := client.Emit("foo", &p); err != nil {
 				panic(err)
 			}
+
+			// p := make(eventsocketclient.Payload)
+			// p["value"] = randomString(32)
+
+			// if err := client.Broadcast(&p); err != nil {
+			// 	panic(err)
+			// }
 		}
 	}
 }
